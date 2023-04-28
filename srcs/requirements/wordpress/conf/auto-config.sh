45a4a59e2543a1a67ac93951417fcebcefc15fe6
Ronan <ronan@debian-gnu-linux-11.localdomain>
2023-04-28 12:26:31 +0200

    not working

---
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -0,0 +1,25 @@
+sleep	10
+
+wp	config create	--allow-root \
+					--dbname=$SQL_DATABASE_NAME \
+					--dbuser=$SQL_USER_NAME \
+					--dbpass=$SQL_USER_PASSWORD \
+					--dbhost=mariadb::3306 \
+					--path='/var/www/wordpress' \
+
+wp	core install	--allow-root \
+					--url=$DOMAIN_NAME/ \
+					--title=$WP_TITLE \
+					--admin_user=$WP_ADMIN_USR \
+					--admin_password=$WP_ADMIN_PWD \
+					--admin_email=$WP_ADMIN_EMAIL \
+					--skip-email \
+
+
+wp user create		--allow-root \
+					$WP_USR $WP_EMAIL \
+					--user_pass=$WP_PWD \
+					--role=author \
+
+#wp redis enable		--allow-root \
+
