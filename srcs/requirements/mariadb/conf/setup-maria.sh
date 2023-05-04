088fd9322f9760d8ae9aba8045d45e81f551faed
Ronan <rpoder@student.42.fr>
2023-05-04 16:36:46 +0200

    working

--- srcs/requirements/mariadb/conf/setup-maria.sh
+++ srcs/requirements/mariadb/conf/setup-maria.sh
@@ -1,36 +1,20 @@
 mysqld &
 
-
-sleep 10
-
-# service mysql start;
-
-# while true
-# do
-#     mysqladmin ping > /dev/null ; echo $? ; mysqladmin ping > /dev/null
-#     if [ $? -eq 0 ]; then
-#         echo "mysqladmin is responding"
-#         break
-#     else
-#         echo "mysqladmin is not responding, retrying..."
-#         sleep 5
-#     fi
-# done
+until mysqladmin ping;
+do
+	sleep 1
+done
 
 if [ ! -e "/var/lib/mysql/.already-exists" ] ; then
-
-touch "/var/lib/mysql/.already-exists"
-mysql -u root -e "CREATE DATABASE ${SQL_DATABASE_NAME};"
-mysql -u root -e "CREATE USER '${SQL_USER_NAME}'@'%' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
-mysql -u root -e "USE '${SQL_DATABASE_NAME}'; GRANT ALL PRIVILEGES ON * TO '${SQL_USER_NAME}'@'%' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
-mysql -u root -e "FLUSH PRIVILEGES"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
-mysql -u root -p${SQL_USER_PASSWORD} -e "FLUSH PRIVILEGES"
-
+	mysql -u root -e "CREATE DATABASE ${SQL_DATABASE_NAME};"
+	mysql -u root -e "CREATE USER '${SQL_USER_NAME}'@'%' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
+	mysql -u root -e "USE '${SQL_DATABASE_NAME}'; GRANT ALL PRIVILEGES ON * TO '${SQL_USER_NAME}'@'%' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
+	mysql -u root -e "FLUSH PRIVILEGES"
+	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
+	mysql -u root -p${SQL_USER_PASSWORD} -e "FLUSH PRIVILEGES"
+	touch "/var/lib/mysql/.already-exists"
 else
-
-echo "mysql is already initialized"
-
+	echo "mysql is already initialized"
 fi
 
 mysqladmin -p${SQL_USER_PASSWORD} shutdown
--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -1,36 +1,39 @@
-sleep	10
-
 if [ ! -e "/var/www/wordpress/wp-config.php" ] ; then
 
-wp	core download	--allow-root \
-					--path='/var/www/wordpress' \
-					--extract
+	wp	core download	--allow-root \
+						--path='/var/www/wordpress' \
+						--extract
 
-wp	config create	--allow-root \
-					--dbname="$SQL_DATABASE_NAME" \
-					--dbuser="$SQL_USER_NAME" \
-					--dbpass="$SQL_USER_PASSWORD" \
-					--dbhost=mariadb:3306 \
-					--path='/var/www/wordpress'
 
-wp	core install	--allow-root \
-					--url=$DOMAIN_NAME/ \
-					--title=$WP_TITLE \
-					--admin_user=$WP_ADMIN_USR \
-					--admin_password=$WP_ADMIN_PWD \
-					--admin_email=$WP_ADMIN_EMAIL \
-					--skip-email \
-					--path='/var/www/wordpress'
+	wp	config create	--allow-root \
+						--dbname="$SQL_DATABASE_NAME" \
+						--dbuser="$SQL_USER_NAME" \
+						--dbpass="$SQL_USER_PASSWORD" \
+						--dbhost=mariadb:3306 \
+						--path='/var/www/wordpress'
 
-wp user create		--allow-root \
-					$WP_USR $WP_EMAIL \
-					--user_pass=$WP_PWD \
-					--role=author \
-					--path='/var/www/wordpress'
+	until wp db check --path=/data/www/wordpress --allow-root --quiet; do
+		sleep 5
+	done
+
+	wp	core install	--allow-root \
+						--url=$DOMAIN_NAME/ \
+						--title=$WP_TITLE \
+						--admin_user=$WP_ADMIN_USR \
+						--admin_password=$WP_ADMIN_PWD \
+						--admin_email=$WP_ADMIN_EMAIL \
+						--skip-email \
+						--path='/var/www/wordpress'
+
+	wp user create		--allow-root \
+						$WP_USR $WP_EMAIL \
+						--user_pass=$WP_PWD \
+						--role=author \
+						--path='/var/www/wordpress'
 
 else
 
-echo "wordpress files already exist"
+	echo "wordpress files already exist"
 
 fi
 #wp redis enable		--allow-root \
